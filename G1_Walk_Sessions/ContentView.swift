716e5c1e328226ae564a3a4b213ffbfc216cef7f
Darshhhhh <contact.shahdarsh@gmail.com>
2025-06-08 12:12:58 -0400

    Initial Commit

---
+++ G1_Walk_Sessions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  G1_Walk_Sessions
+//
+//  Created by Darsh on 2025-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ G1_Walk_Sessions/G1_Walk_SessionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  G1_Walk_SessionsApp.swift
+//  G1_Walk_Sessions
+//
+//  Created by Darsh on 2025-06-08.
+//
+
+import SwiftUI
+
+@main
+struct G1_Walk_SessionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
